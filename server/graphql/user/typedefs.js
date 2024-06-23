bd2ccc5fe6ba0dc5edcad824f3d8658883199828
ANJSACHAN <theanjalisachan@gmail.com>
2024-06-23 21:48:43 +0530

    update User typedef

--- server/graphql/user/typedefs.js
+++ server/graphql/user/typedefs.js
@@ -1,1 +1,19 @@
-export const typeDefs = ``;
+export const typeDefs = `
+type User {
+     id: Int
+     type: Int
+     email: String
+     displayName: String
+     displayImg: String
+     about: String
+     Posts: [Post]
+     PostLikes: [PostLike]
+     GroupMemberships: [GroupMembership]
+     darkMode: Boolean
+     lastLogin: Date
+     streak: Int
+     notifications: [String]
+   }
+
+
+`;
