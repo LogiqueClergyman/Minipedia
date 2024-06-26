a429978eb87f9b100dd2076927a291a0dc715811
ANJSACHAN <theanjalisachan@gmail.com>
2024-06-26 14:08:43 +0530

    Initial Routing

--- server/graphql/post/index.js
+++ server/graphql/post/index.js
@@ -3,4 +3,4 @@ import { queries } from './queries';
 import { typeDefs } from './typedef';
 import { resolvers } from './resolvers';
 
-export const User = {typeDefs , queries , mutations , resolvers};
+export const Post = {typeDefs , queries , mutations , resolvers};
